bb8eed52bb851aca108b4a56df5f53ba0b693d3a
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2018-09-29 11:37:04 +0200

    ---------- MasterMind game ----------
    What changed:
    The randomCode method got improve.
    The validation method got changed.
    
    What is new:
    The number of attempts is shown to the user.
    The user haves the possibility to play again if he/she wins or loses.

--- MasterMind.java
+++ MasterMind.java
@@ -1,41 +1,57 @@
 package test;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MasterMind {
 	private int[] randomCode = new int[4];
 	private Scanner userInput;
-	private int cpt = 1;
-	private boolean gameStatus = false;
-	
+	Scanner scAnswer;
+	private int cpt = 8;
+	private int y = 0;
+	private String userAnswer;
+
 	public void MasterGameMind() {
-		this.randomCode();
-		System.out.println("The random code : "+randomCode[0]+randomCode[1]+randomCode[2]+randomCode[3]);
-		int[] userInputCode = getUserInput();
-		// System.out.println("test : "+userInputCode[3]+userInputCode[2]+userInputCode[1]+userInputCode[0]);
-		validation(userInputCode);
+		/* Generate a random code
+		 * get the user input
+		 * compare the user input with the random generate code
+		 * if the user does not find the code before the number of attempts expire, he/she have the option to play again*/
+		do {
+			this.randomCode();
+			do {
+				if (y == 4) {
+					System.out.println("	Code confirm !!!\n\n");
+					break;
+				}
+				System.out.println("\nThe random code : "+randomCode[0]+randomCode[1]+randomCode[2]+randomCode[3]);
+				int[] userInputCode = getUserInput();
+				validation(userInputCode);
+			}while(this.cpt >= 1);
+			System.out.println("Game over");
+			cpt=8;
+			y=0;
+
+			System.out.println("Would you like to play again ? yes/no");
+			scAnswer = new Scanner(System.in);
+			userAnswer = scAnswer.nextLine();
+		}while(userAnswer.trim().equals("yes"));
+		
+		/* if the user wants to close the game,
+		 * close all the Scanner,
+		 * the user will be notify and the games shut down*/
+		scAnswer.close();
+		userInput.close();
+		System.out.println("\n\n\nSee you soon !!!");
+		System.exit(0);
 	}
-	
-	private void startAgain() {
-		System.out.println("The random code : "+randomCode[0]+randomCode[1]+randomCode[2]+randomCode[3]);
-		cpt++;
-		int[] userInputCode = getUserInput();
-		validation(userInputCode);
-	}
-	
-	private void gameOver() {
-		if (gameStatus) {
-			cpt=0;
-			userInput.close();
-		}
-	}
-	
+
 	private void randomCode() {
 		//generate a random code of 4 digits and store them
 		for (int i = 0; i < randomCode.length; i++) {
 			Random rand = new Random();
-			randomCode[i] = rand.nextInt(9);
+			randomCode[i] = rand.nextInt((9-1)+1)+1;
+			
 		}
 	}
 	
@@ -43,7 +59,7 @@ public class MasterMind {
 		/*- get user input and store it
 		 *- get each digits from the user input */
 		userInput = new Scanner(System.in);
-		System.out.println(cpt+" try. Enter your code:");
+		System.out.println("Number of attempt(s) left: "+cpt+"\nEnter your code:");
 		int userCode = userInput.nextInt();
 		
 		int userCodePart1 = 0;
@@ -56,70 +72,35 @@ public class MasterMind {
 		userCodePart3 = (userCode % 1000) / 100;
 		userCodePart4 = (userCode % 10000) / 1000;
 		
-		int [] userInputCode = {userCodePart1, userCodePart2, userCodePart3, userCodePart4};
+		int [] userInputCode = {userCodePart4, userCodePart3, userCodePart2, userCodePart1};
 		return userInputCode;
 	}
 	
-	private void validation(int[] userInputCode) { 
+	private void validation(int[] userInputCode) {
 		/*- compare the generated code and user code
 		 *- if both codes are exact... notify the user "code confirmed
 		 *- else if there are digits from the user input that exist but not in the correct order... notify the user
 		 *-			else the digits from user does not exist... notify the user"*/
 		
-		int fullRandomCode = randomCode[0]+randomCode[1]+randomCode[2]+randomCode[3];
-		int fullUserInputCode[] = new int[4];
-		/*
-		fullUserInputCode[0] = userInputCode[3];
-		fullUserInputCode[1] = userInputCode[2];
-		fullUserInputCode[2] = userInputCode[1];
-		fullUserInputCode[3] = userInputCode[0];*/
-		
-		
-		//store the user input in the correct order
-		for(int i=0, j=3; (i<4) && (j>-1); i++, j--) {
-			fullUserInputCode[i] = userInputCode[j];
-			//System.out.println("test: "+fullUserInputCode[i]+"___"+userInputCode[j]);
+		System.out.println("Results...:  ");
+		for (int i = 0; i < userInputCode.length; i++) {
+			if (randomCode[i] == userInputCode[i]) {
+				System.out.print(randomCode[i]);
+				y++;
+			}else if(Arrays.toString(randomCode).contains( String.valueOf(userInputCode[i]) )) {
+				System.out.print(".");
+			}else {
+				System.out.print("X");
+			}
 		}
 		
-		int userCodeId=0;
-		
-		if (fullRandomCode == (fullUserInputCode[0]+fullUserInputCode[1]+fullUserInputCode[2]+fullUserInputCode[3])) {
-			// if the user code matches with the random code
-			System.out.println("Code confirm ");
-		}else  {
-			//check each digits that the user entered
-			System.out.println("Results...:\n");
-			for (int i : randomCode) {
-				if (i == fullUserInputCode[userCodeId]) {
-					//if the digit from the random code matches the digit from the user input AT THE SAME INDEX OVER BOTH ARRAYS
-					 System.out.print(fullUserInputCode[userCodeId]);
-					 
-				}else {
-					//check the user digit at x index if it exists in other indexes in the random code array
-					//not really working
-					int checkDigit=0;
-					for(int check : randomCode) {
-						if(fullUserInputCode[checkDigit] == check) {
-							//checkDigit++;
-							//if (checkDigit > 2) {
-							//System.out.print("("+fullUserInputCode[userCodeId]+")");
-							System.out.print(".");
-							//}
-						}
-					}
-					//System.out.print("["+fullUserInputCode[userCodeId]+"]");
-					System.out.print("X");
-				}
-				userCodeId++;
-			}
-			
-			if(cpt != 8) {
-				System.out.println("\nThe code you entered is incorrect, please try again.\n");
-				startAgain();
-			}else {
-				System.out.println("Game Over !!!");
-				gameOver();
-			}
+		//if the code is not confirmed. the user will try again with a limited number of attempts and he will be notify
+		cpt--;
+		if (y != 4) {
+			y=0;
+			System.out.println("\nThe code you entered is incorrect, please try again.\n\n");
 		}
 	}
+	
+	
 }
--- Test.java
+++ Test.java
@@ -4,12 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		/*
-		 * le code : 7 2 3 3
-		 * votre code a 4 chiffre
-		 *  */
-		
 		MasterMind mm = new MasterMind();
 		mm.MasterGameMind();
 		}
